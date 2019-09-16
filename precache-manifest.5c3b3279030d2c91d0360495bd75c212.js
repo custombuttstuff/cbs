307e1ab542bada1a4680ec960972e025bf5400db
Nick Parker <nmp67@cornell.edu>
2019-09-16 19:03:35 -0400

    Updates

---
+++ precache-manifest.5c3b3279030d2c91d0360495bd75c212.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe5fa28553b508e206064fa2769d181b",
+    "url": "/cbs/index.html"
+  },
+  {
+    "revision": "932083749875928d98d7",
+    "url": "/cbs/static/css/main.c84a955c.chunk.css"
+  },
+  {
+    "revision": "0d3bebcc71de0a6cf3cd",
+    "url": "/cbs/static/js/2.fca22e59.chunk.js"
+  },
+  {
+    "revision": "932083749875928d98d7",
+    "url": "/cbs/static/js/main.84718a28.chunk.js"
+  },
+  {
+    "revision": "c7db4d9e0f7c720fab68",
+    "url": "/cbs/static/js/runtime~main.85b608e1.js"
+  },
+  {
+    "revision": "0a6e3db45b63d9c29d4b1309bbda626d",
+    "url": "/cbs/static/media/design1.0a6e3db4.png"
+  },
+  {
+    "revision": "54c4f7bac6bd8524f2c27c0c0e9733a9",
+    "url": "/cbs/static/media/design2.54c4f7ba.png"
+  }
+]);
